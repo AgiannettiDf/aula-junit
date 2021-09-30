396e19d5672b096bdb54b9ef66d392341b381e0f
Armando Giannetti <armando.giannetti@outlook.com>
2021-09-30 14:20:34 -0300

    Add Tests Financing

---
+++ src/tests/entities/FinancingTests.java
@@ -0,0 +1,94 @@
+package tests.entities;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import entities.Financing;
+
+public class FinancingTests {
+
+	@Test
+	public void constructorShouldCreateObjectWhenValidData() {
+		//arrange
+		//action
+		Financing fin = new Financing(100000.0, 2000.0, 80);
+		//assert
+		Assertions.assertEquals(fin.getTotalAmount(), 100000.00);
+		Assertions.assertEquals(fin.getIncome(), 2000.00);
+		Assertions.assertEquals(fin.getMonths(), 80);
+	}
+	
+	@Test
+	public void constructorShouldThrowIllegalArgumentExceptionWhenInvalidData() {
+		Assertions.assertThrows(IllegalArgumentException.class, ()-> {
+			Financing fin = new Financing(100000.0, 2000.0, 20);
+		});
+	}
+	
+	@Test
+	public void setTotalAmountShouldSetDataWhenValidData() {
+		//arrange
+		//action
+		Financing fin = new Financing(100000.0, 2000.0, 80);
+		fin.setTotalAmount(90000.0);
+		//assert
+		Assertions.assertEquals(fin.getTotalAmount(), 90000.00);
+	}
+	
+	@Test
+	public void setTotalAmountShouldThrowIllegalArgumentExceptionWhenInvalidData() {
+		Assertions.assertThrows(IllegalArgumentException.class, ()-> {
+			Financing fin = new Financing(100000.0, 2000.0, 80);
+			fin.setTotalAmount(110000.0);
+		});
+	}
+	
+	@Test
+	public void setIncomeShouldSetDataWhenValidData() {
+		//arrange
+		//action
+		Financing fin = new Financing(100000.0, 2000.0, 80);
+		fin.setIncome(3000.0);
+		//assert
+		Assertions.assertEquals(fin.getIncome(), 3000.00);
+	}
+	
+	@Test
+	public void setIncomeShouldThrowIllegalArgumentExceptionWhenInvalidData() {
+		Assertions.assertThrows(IllegalArgumentException.class, ()-> {
+			Financing fin = new Financing(100000.0, 2000.0, 80);
+			fin.setIncome(1000.0);
+		});
+	}
+	
+	@Test
+	public void setMonthsShouldSetDataWhenValidData() {
+		//arrange
+		//action
+		Financing fin = new Financing(100000.0, 2000.0, 80);
+		fin.setMonths(90);
+		//assert
+		Assertions.assertEquals(fin.getMonths(), 90);
+	}
+	
+	@Test
+	public void setMonthsShouldThrowIllegalArgumentExceptionWhenInvalidData() {
+		Assertions.assertThrows(IllegalArgumentException.class, ()-> {
+			Financing fin = new Financing(100000.0, 2000.0, 80);
+			fin.setMonths(70);
+		});
+	}
+	
+	@Test
+	public void entryShouldCalculateEntryCorrectly() {
+		Financing fin = new Financing(100000.0, 2000.0, 80);
+		Assertions.assertEquals(fin.entry(), 20000.00);
+	}
+	
+	@Test
+	public void quotaShouldCalculateQuotaCorrectly() {
+		Financing fin = new Financing(100000.0, 2000.0, 80);
+		Assertions.assertEquals(fin.quota(), 1000.00);
+	}
+
+}
